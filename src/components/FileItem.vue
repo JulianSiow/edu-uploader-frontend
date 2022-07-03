1e0cefddb1e5f1ab3da2e62a43cc1c57a404b6ab
Julian Siow <julian.k.siow@gmail.com>
2022-07-03 17:37:44 +0200

    allows for downloading uploaded files with a button

--- src/components/FileItem.vue
+++ src/components/FileItem.vue
@@ -1,13 +1,42 @@
 <template>
-  <li>{{ fileName }}</li>
+  <li>
+    {{ fileName }}
+    <button @click="downloadFile">Download File</button>
+  </li>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   props: {
     fileName: String,
+  },
+
+  setup(props) {
+    const downloadFile = () => {
+      axios
+        .get(`http://localhost:8080/files/${props.fileName}`)
+        .then((res) => {
+          console.log(res);
+          var fileURL = window.URL.createObjectURL(new Blob([res.data]));
+          var fileLink = document.createElement("a");
+
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", props.fileName);
+          document.body.appendChild(fileLink);
+
+          fileLink.click();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    return {
+      downloadFile,
+    };
   },
 });
 </script>
